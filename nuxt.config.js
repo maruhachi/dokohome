4c15cc5271a734fa1b719d4503b5aa56278eaed6
maruhachi <konchan1129@gmail.com>
2018-10-07 14:31:04 +0900

    :tada: initial commit. vue-cli init.

---
+++ nuxt.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'dokohome',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'find your home base.' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    ** https://is.gd/TZZAbK
+    */
+    extend (config) {
+      if (process.server && process.browser) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
