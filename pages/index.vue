8f0fed93c37bc849e685d69062e0ab3aaf794c22
maruhachi <konchan1129@gmail.com>
2019-08-13 03:28:11 +0900

    googleMapでのピン指し

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,22 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo />
-      <div>
-        <p>地図！</p>
-        <div id="map-wrap">
-          <no-ssr>
-            <l-map :zoom="13" :center="[38.2, 140.8]">
-              <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-              <!-- <l-marker :lat-lng="[47.413220, -1.219482]"></l-marker> -->
-            </l-map>
-          </no-ssr>
-        </div>
-      </div>
-    </div>
+    <v-layout row justify-center align-center>
+      <GmapMap
+        :center="{lat:10, lng:10}"
+        :zoom="7"
+        map-type-id="terrain"
+        style="width: 500px; height: 300px"
+      >
+        <GmapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center=m.position"
+        />
+      </GmapMap>
+    </v-layout>
   </section>
 </template>
 
@@ -21,6 +24,14 @@
 import AppLogo from "~/components/AppLogo.vue";
 
 export default {
+  data() {
+    return {
+      markers: [
+        { position: { lng: 10.2, lat: 10 } },
+        { position: { lng: 10.1, lat: 10 } }
+      ]
+    };
+  },
   components: {
     AppLogo
   }
@@ -28,11 +39,6 @@ export default {
 </script>
 
 <style>
-#map-wrap {
-  height: 100vh;
-  width: 200vh;
-}
-
 .container {
   min-height: 100vh;
   width: 100%;
@@ -40,28 +46,6 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
 
